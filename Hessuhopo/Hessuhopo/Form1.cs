ef8f80b14457c899be4cf4bab99edcd0067c7f71
Timo Tyrväinen <gr186703@gradia.fi>
2019-11-28 13:29:42 +0200

    HessuHopo

---
+++ Hessuhopo/Hessuhopo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hessuhopo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Point piste = new Point(0, 0);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+       
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap viidakko = new Bitmap(Properties.Resources.Hopo);
+            viidakko.MakeTransparent();
+            e.Graphics.DrawImage(viidakko, piste);
+            DoubleBuffered = true;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                piste = e.Location;
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                piste = e.Location;
+                Invalidate();
+            }
+        }
+    }
+}
---
+++ Hessuhopo/Hessuhopo/Properties/Recourses.cs
@@ -0,0 +1,7 @@
+﻿namespace Hessuhopo.Properties
+{
+    internal class Recourses
+    {
+        public static object Hopo { get; internal set; }
+    }
+}
